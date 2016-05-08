f9832ba6f6e3513305bd96184830f7e40058220a
Kalli Martin <kalli8@vt.edu>
2016-05-08 17:56:33 -0400

    view inflation for dynamically setting content of content_base

--- LocalShare/mobile/src/main/java/com/example/kalli/localshare/AvailableItems.java
+++ LocalShare/mobile/src/main/java/com/example/kalli/localshare/AvailableItems.java
@@ -1,9 +1,28 @@
 package com.example.kalli.localshare;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
 /**
  * Created by kalli on 5/8/16.
  */
 public class AvailableItems extends BaseActivity {
 
+    FrameLayout r;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        r = (FrameLayout) findViewById(R.id.content_base);
+        LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = vi.inflate(R.layout.activity_available_items, null);
+        ViewGroup insertPoint = (ViewGroup) findViewById(R.id.content_base);
+        insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+
+    }
 
 }
