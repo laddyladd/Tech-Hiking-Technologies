b7207415b08d0a900e9e216ee70119644333a651
Kalli Martin <kalli8@vt.edu>
2016-05-08 17:16:51 -0400

    Made new app LocalShare

---
+++ LocalShare/mobile/src/main/java/com/example/kalli/localshare/AvailableItems.java
@@ -0,0 +1,9 @@
+package com.example.kalli.localshare;
+
+/**
+ * Created by kalli on 5/8/16.
+ */
+public class AvailableItems extends BaseActivity {
+
+
+}
---
+++ LocalShare/mobile/src/main/java/com/example/kalli/localshare/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.kalli.localshare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent (this, AvailableItems.class);
+        startActivity(i);
+
+    }
+}
