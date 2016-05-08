0d413b4b4fefc5a3ab42418ee11dfb1355effbd6
Kalli Martin <kalli8@vt.edu>
2016-05-08 18:27:48 -0400

    layer inflation

--- LocalShare/mobile/src/main/java/com/example/kalli/localshare/AvailableItems.java
+++ LocalShare/mobile/src/main/java/com/example/kalli/localshare/AvailableItems.java
@@ -1,11 +1,13 @@
 package com.example.kalli.localshare;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 /**
  * Created by kalli on 5/8/16.
@@ -20,6 +22,10 @@ public class AvailableItems extends BaseActivity {
         r = (FrameLayout) findViewById(R.id.content_base);
         LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View v = vi.inflate(R.layout.activity_available_items, null);
+        TextView textView = (TextView) v.findViewById(R.id.text);
+        textView.setText("Summer");
+        textView.setTextColor(Color.BLACK);
+
         ViewGroup insertPoint = (ViewGroup) findViewById(R.id.content_base);
         insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
 
